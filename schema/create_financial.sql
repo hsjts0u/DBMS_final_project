c79d6d868b36b6a6caba20e2d49895db3c6a44d4
tsenyen <tsenyen.cs08@nctu.edu.tw>
2021-05-23 17:10:29 +0800

    0523-ty-add_financial_table

---
+++ schema/create_financial.sql
@@ -0,0 +1,12 @@
+CREATE TABLE financial(
+    ticker varchar(20) NOT NULL,
+    date date,
+    research_development int,
+    net_income int,
+    gross_profit int,
+    ebit int,
+    operating_income int,
+    interest_expense int,
+    primary key (ticker)
+    
+);
