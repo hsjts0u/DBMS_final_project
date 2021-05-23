e149f7430219a3dfa6c8177e5b7c97f1940845e2
tsenyen <tsenyen.cs08@nctu.edu.tw>
2021-05-23 23:53:04 +0800

    0523-ty-fetch_financial

---
+++ financial_insert_data.py
@@ -0,0 +1,25 @@
+import mysql.connector
+import yfinance as yf
+import numpy as np
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="db_stock"
+)
+
+cursor = mydb.cursor()
+sql = "INSERT INTO financial VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+dic = {} 
+
+msft = yf.Ticker("MSFT")
+Fin = ('Research Development', 'Net Income', 'Gross Profit', 'Ebit', 'Operating Income', 'Interest Expense')
+for i in Fin:
+    dic[i] = msft.financials.loc[i]
+
+for i in range(len(dic['Research Development'])):
+   # print(dic['Research Development'].index[i])
+    val = ( 'MSFT', dic['Research Development'].index[i],dic['Research Development'][i], dic['Net Income'][i], dic['Gross Profit'][i], dic['Ebit'][i], dic['Operating Income'][i], dic['Interest Expense'][i] )
+    cursor.execute(sql,val)
+    mydb.commit()
--- schema/create_financial.sql
+++ schema/create_financial.sql
@@ -1,11 +1,11 @@
 CREATE TABLE financial(
     ticker varchar(20) NOT NULL,
     date date,
-    research_development int,
-    net_income int,
-    gross_profit int,
-    ebit int,
-    operating_income int,
-    interest_expense int,
+    research_development bigint,
+    net_income bigint,
+    gross_profit bigint,
+    ebit bigint,
+    operating_income bigint,
+    biginterest_expense bigint,
     primary key (ticker)
 );
