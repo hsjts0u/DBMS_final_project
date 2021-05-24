1c58214ba4fc62b670b0bfae89b74c888d888ddf
tsenyen <tsenyen.cs08@nctu.edu.tw>
2021-05-24 18:53:04 +0800

    0524-ty-fetch_earning

--- fetch_earning.py
+++ fetch_earning.py
@@ -1,24 +1,31 @@
 import yfinance as yf
-from pandas import DataFrame
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="db_stock"
+)
+
+cursor = mydb.cursor()
 
 def _fetch_earning(ticker, mydb, mycursor):
 
     sql = "INSERT INTO earning_data (ticker, year, earning) VALUES (%s, %s, %s)"
-
     msft = yf.Ticker(ticker)
     ern = msft.earnings
+    for i in range(len(ern.index)):
+        query = "SELECT * FROM earning_data WHERE ticker='"+ ticker +"' AND year='" + str(ern.index[i].item()) + "'"
+        cursor.execute(query)
+        result = cursor.fetchall()
 
+        if not result:
+            val = ( ticker, ern.index[i].item(), ern.loc[ern.index[i],'Earnings'].item() )
+            mycursor.execute(sql, val)
+            mydb.commit()
 
-    val = [   (ticker, ern.index[0].item(), ern.loc[ern.index[0],'Earnings'].item()),
-                (ticker, ern.index[1].item(), ern.loc[ern.index[1],'Earnings'].item()),
-                (ticker, ern.index[2].item(), ern.loc[ern.index[2],'Earnings'].item()),
-                (ticker, ern.index[3].item(), ern.loc[ern.index[3],'Earnings'].item())
-            ]
-
-    mycursor.executemany(sql, val)
-    mydb.commit()
-
-
+_fetch_earning('MSFT', mydb, cursor)
 
 
 
