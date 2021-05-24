62a630f3785d56467afe78d4347b2fa3ccc1ceb7
tsenyen <tsenyen.cs08@nctu.edu.tw>
2021-05-24 17:20:12 +0800

    0523-ty-fetch_financial_update_2

--- financial_insert_data.py
+++ financial_insert_data.py
@@ -1,14 +1,15 @@
 import yfinance as yf
 import numpy as np
+import mysql.connector 
 
-#mydb = mysql.connector.connect(
-    #host="localhost",
-    #user="root",
-    #password="",
-    #database="db_stock"
-#)
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="db_stock"
+)
 
-#cursor = mydb.cursor()
+cursor = mydb.cursor()
 
 def _fetch_financial(ticker, mydb, mycursor):
     sql = "INSERT INTO financial VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
@@ -23,7 +24,12 @@ def _fetch_financial(ticker, mydb, mycursor):
     for i in range(len(dic['Research Development'])):
         temp = dic['Research Development'].index[i].strftime('%Y-%m-%d')
         val = ( ticker, temp, dic['Research Development'][i], dic['Net Income'][i], dic['Gross Profit'][i], dic['Ebit'][i], dic['Operating Income'][i], dic['Interest Expense'][i] )
-        cursor.execute(sql, val)
-        mydb.commit()
+       
+        testing = "SELECT * FROM financial WHERE ticker='"+ ticker +"' AND date='" + temp + "'"
+        cursor.execute(testing)
+        result = cursor.fetchall()
+        if not result :
+            cursor.execute(sql, val)
+            mydb.commit()
         
-
+_fetch_financial('AMZN',mydb, cursor)
