bf49e7ab492c2e0ba0e8e946f7675c52fe3dfb98
tsenyen <tsenyen.cs08@nctu.edu.tw>
2021-05-25 19:52:26 +0800

    0525-ty-fetch_history_update

--- fetch_history.py
+++ fetch_history.py
@@ -1,15 +1,15 @@
-import mysql.connector
 import yfinance as yf
 import datetime
 import time
 import numpy as np
 import pandas as pd
+import mysql.connector
 
 #mydb = mysql.connector.connect(
-  #host="localhost",
-  #user="",
-  #password="",
-  #database="db_stock"
+#  host="localhost",
+#  user="root",
+#  password="",
+#  database="db_stock"
 #)
 
 #cursor = mydb.cursor()
@@ -17,14 +17,19 @@ import pandas as pd
 def _fetch_history(ticker, mydb, mycursor):
     sql = "INSERT INTO history_stock_data VALUES (%s, %s, %s, %s, %s, %s, %s)"
     dic = {} 
-    
+        
     symb = yf.Ticker(ticker).history(start = "2000-01-03", end = datetime.date.today())
    
     for i in range(len(symb.index)):
         temp = symb.index[i].strftime('%Y-%m-%d')
-        val = ( ticker, temp, symb['Open'][i].item(), symb['High'][i].item(), symb['Low'][i].item(), symb['Close'][i].item(), symb['Volume'][i].item() )
-        cursor.execute(sql, val)
-        mydb.commit()
+        query = "SELECT * FROM history_stock_data WHERE ticker='"+ticker+"' AND day='"+temp+"'"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        
+        if not result :
+            val = ( ticker, temp, symb['Open'][i].item(), symb['High'][i].item(), symb['Low'][i].item(), symb['Close'][i].item(), symb['Volume'][i].item() )
+            cursor.execute(sql, val)
+            mydb.commit()
     
 #_fetch_history('AAPL', mydb,cursor)
 
