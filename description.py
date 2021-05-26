2578f40da0ac2c19694cd6444bcd4a2ae4088c33
Jay Tsou <hsjts0u@gmail.com>
2021-05-26 21:24:40 +0800

    5/26-hsjts0u-decription_change

--- description.py
+++ description.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.graph_objects as go
 from GoogleNews import GoogleNews
 
 def objects(ticker, mydb):
@@ -37,6 +38,28 @@ def objects(ticker, mydb):
         mycursor.execute(query)
         result = mycursor.fetchall()
         st.write(f"52 Week Change: {result[0][0]}")
+        
+        st.subheader("Safety Index")
+        query = "SELECT dividendRate / beta FROM info_data WHERE ticker='" + ticker +"'"
+        mycursor.execute(query)
+        result = mycursor.fetchall()
+        si = 0
+        if result[0][0] > 0:
+            si = max(si, result[0][0])
+        
+        fig = go.Figure(go.Indicator(
+                        mode = "gauge+number",
+                        value = round(si, 2),
+                        domain = {'x': [0, 1], 'y': [0, 1]},
+                        title = {'text': "Safety Index"},
+                        gauge = {'axis': {'range': [0, 24]},
+                                 'bar': {'color': "white"},
+                                 'steps' : [
+                                    {'range': [0, 8], 'color': "#e3361b"}, 
+                                    {'range': [8, 16], 'color': "#f5e556"}, 
+                                    {'range': [16, 24], 'color': "#45c449"}]} 
+                        ))
+        st.plotly_chart(fig)
         
         st.header(f'News on {ticker}')
         googlenews = GoogleNews('en')
