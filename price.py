c35735bc4eaf38e59fb74947cf3e8541d54dbd36
tsenyen <tsenyen.cs08@nctu.edu.tw>
2021-05-26 20:21:52 +0800

    0526-ty-price_chart

--- price.py
+++ price.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import plotly.graph_objects as go
 import pandas as pd
+import plotly.express as px
 from datetime import timedelta, datetime
 
 def objects(ticker, mydb):
+    st.header('Historical Prices')
     num_days = st.sidebar.slider('Number of days', 0, 30, 0)
     num_months = st.sidebar.slider('Number of months', 1, 12, 1)
     num_years = st.sidebar.slider('Number of years', 0, 20, 0)
@@ -15,11 +17,13 @@ def objects(ticker, mydb):
     mycursor.execute(query)
     result = mycursor.fetchall()
     DF = pd.DataFrame(result, columns=['Date','Open','High','Low','Close','Volume'])
-    fig = go.Figure()
+    layout = go.Layout(title='Candlestick', width=800, height=600)
+    fig2 = px.line(DF, x='Date', y='Close',title='Time Series', width=800, height=600)
+    fig = go.Figure(layout=layout)
     fig.add_trace(go.Candlestick(x=DF['Date'], open=DF['Open'], high=DF['High'], low=DF['Low'], close=DF['Close']))
     DF.reset_index(inplace=False)
     DF.set_index("Date", inplace=True)
-    st.line_chart(DF["Close"])
+    st.plotly_chart(fig2)
     st.plotly_chart(fig)
     st.table(DF)
     
