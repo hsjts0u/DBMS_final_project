f725f5dc58b90a0604fd928fcb30ace4421acb79
Jay Tsou <hsjts0u@gmail.com>
2021-05-26 20:29:00 +0800

    5/26-hsjts0u-fetch_change

--- fetch.py
+++ fetch.py
@@ -25,13 +25,32 @@ def _fetch_info(ticker, mydb, mycursor):
     query = "SELECT * FROM info_data WHERE ticker='"+ ticker +"'"
     mycursor.execute(query)
     result = mycursor.fetchall()
+    
+    info_dict = yf.Ticker(ticker).info
+    
     if not result:   
         sql = "INSERT INTO info_data VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        info_dict = yf.Ticker(ticker).info
         
         val = (ticker, info_dict['dividendRate'], info_dict['beta'], info_dict['52WeekChange'], info_dict['shortName'],info_dict['longName'],
         info_dict['forwardEps'], info_dict['bookValue'], info_dict['priceToBook'], info_dict['shortRatio'], info_dict['longBusinessSummary'],
         info_dict['logo_url'])
+        
+        mycursor.execute(sql, val)
+        mydb.commit()
+    else:
+        sql = """
+                UPDATE info_data 
+                SET 
+                    dividendRate = %s, beta = %s, 52WeekChange = %s, 
+                    shortName = %s, longName = %s, forwardEps = %s, 
+                    bookValue = %s, priceToBook = %s, shortRatio = %s,
+                    longBusinessSummary = %s, logo_url = %s
+                WHERE ticker = %s
+              """
+        
+        val = (info_dict['dividendRate'], info_dict['beta'], info_dict['52WeekChange'], info_dict['shortName'],info_dict['longName'],
+        info_dict['forwardEps'], info_dict['bookValue'], info_dict['priceToBook'], info_dict['shortRatio'], info_dict['longBusinessSummary'],
+        info_dict['logo_url'], ticker)
         
         mycursor.execute(sql, val)
         mydb.commit()
