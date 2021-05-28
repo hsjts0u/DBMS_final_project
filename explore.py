1b201748a74ea7a0cc1641e983e605f3dd019c50
tsenyen <tsenyen.cs08@nctu.edu.tw>
2021-05-28 19:37:10 +0800

    0528-ty-explore

--- explore.py
+++ explore.py
@@ -3,31 +3,29 @@ import pandas as pd
 
 def objects(mydb):
     mycursor = mydb.cursor()
-    sql_cat = "SELECT DISTINCT category_name FROM stock_list"
-    sql_exchange = "SELECT DISTINCT exchange FROM stock_list"
+    sql_cat = "SELECT DISTINCT category_name FROM stock_list ORDER BY category_name ASC"
+    sql_exchange = "SELECT DISTINCT exchange FROM stock_list ORDER BY exchange ASC"
+    sql_country = "SELECT DISTINCT country FROM stock_list ORDER BY country ASC"
     mycursor.execute(sql_cat)
     Cat = mycursor.fetchall()
     mycursor.execute(sql_exchange)
     Exc = mycursor.fetchall()
-    options_country = st.multiselect(
-     'Select Country',
-     ['Canada','India','China','South Korea','Hong Kong','Malaysia','France','Taiwan','Germany', 'United Kingdom',
-      'Mexico','Spain','Sweden','Indonesia','Australia','Thailand','Switzerland','Ireland','Singapore','USA','Italy','Argentina',
-      'Greece','Denmark','Netherlands','Brazil','New Zealand','Venezuela','Austria','Belgium','Israel','Qatar','Russia','Norway',
-      'Finland','Turkey','Portugal','Lithuania','Estonia','Latvia','Icelandreen'],
-     ['Taiwan']
-     )
+    mycursor.execute(sql_country)
+    Coun = mycursor.fetchall()
     c = [ i[0] for i in Cat]
     e = [ i[0] for i in Exc]
+    co = [ i[0] for i in Coun]
+    options_country = st.multiselect('Select Country', co, ['Taiwan'])
     options_category = st.multiselect('Select Category', c)
     options_exchange = st.multiselect('Select Exchange', e)
+  
 
     cat_name = ['category_name' for i in range(len(options_category))]
     cou_name = ['country' for i in range(len(options_country))]
     exc_name = ['exchange' for i in range(len(options_exchange))]
     options = list(zip(cou_name, options_country)) + list(zip(cat_name, options_category)) + list(zip(exc_name, options_exchange))
     
-    orderby = st.radio("Data is order by ", ('Country', 'Category', 'Exchange'))
+    orderby = st.radio("Data is ordered by ", ('Country', 'Category', 'Exchange'))
     if orderby == 'Country':
         order = 'country'
     elif orderby == 'Category':
